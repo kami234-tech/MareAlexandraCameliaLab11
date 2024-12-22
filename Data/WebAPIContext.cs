bb7d3ccb59885b7db610b84aa9e6c507b141cd24
Mare Alexandra-Camelia <marekami234@gmail.com>
2024-12-22 19:46:41 +0200

    Add project files.

---
+++ Data/WebAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MareAlexandraCameliaLab11.Models;
+
+namespace WebAPI.Data
+{
+    public class WebAPIContext : DbContext
+    {
+        public WebAPIContext (DbContextOptions<WebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MareAlexandraCameliaLab11.Models.ShopList> ShopList { get; set; } = default!;
+    }
+}
